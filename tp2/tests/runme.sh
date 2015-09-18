d337d276e3b3e9456442cf1ec436c9c763196011
tomas <tomasmussi@gmail.com>
2015-09-18 14:47:10 -0300

    Toques a los scripts para poder correr con valgrind

--- tp2/tests/runme.sh
+++ tp2/tests/runme.sh
@@ -4,15 +4,19 @@ testcase=$1
 echo "[[[test case: $testcase]]]"
 port=$(./free_port.sh 3000)
 
-rm -R runenv/*
+rm -Rf runenv/*
+$(rm -Rf backup/$testcase)
+
 if [ $? -ne 0 ] 
 then
   echo "FAIL: rm failed"
   exit 1
 fi
 mkdir runenv/expected
+$(mkdir backup/$testcase)
 
-cp ../bin/client ../bin/server runenv
+cp ../bin/client runenv/client
+cp ../bin/server runenv/server
 
 cp -f $testcase/in.zip runenv/
 cp -f $testcase/out.zip runenv/expected
@@ -32,6 +36,14 @@ then
   exit 1
 fi
 
+if [ $2 == "valgrind" ]
+then
+	for f in $(ls valgrind*)
+	do
+		$(mv $f ../backup/$testcase/$f)
+	done
+fi
+
 for s in $(ls *.out __*)
 do 
     diff $s expected/$s
@@ -40,6 +52,16 @@ do
       echo "FAIL: $s"
       exit 1
     fi  
+done
+
+for f in $(ls ../backup/$testcase)
+do
+	cat ../backup/$testcase/$f | grep -q "All heap blocks were freed -- no leaks are possible"
+	if [ $? -ne 0 ]
+	then
+		echo "VALGRIND FAIL: $f"
+		exit 1
+	fi
 done
 
 cd ..
