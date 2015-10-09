7cbd469acbcd1d495c86044489baf18271c5eb56
tomas <tomasmussi@gmail.com>
2015-10-09 15:43:38 -0300

    Primeros ejemplos con gtkmm

---
+++ pruebas/gtk/ejemplo1.cpp
@@ -0,0 +1,9 @@
+
+#include <gtkmm.h>
+
+int main(int argc, char *argv[]) {
+	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv,"org.gtkmm.examples.base");
+	Gtk::Window window;
+	window.set_default_size(200, 200);
+	return app->run(window);
+}
---
+++ pruebas/gtk/ejemplo2.cpp
@@ -0,0 +1,43 @@
+#include <gtkmm.h>
+#include <iostream>
+
+Gtk::Dialog* pDialog = 0;
+
+static void on_button_clicked()
+{
+	if(pDialog)
+		pDialog->hide(); //hide() will cause main::run() to end.
+}
+
+int main (int argc, char **argv) {
+	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+
+	//Load the GtkBuilder file and instantiate its widgets:
+	Glib::RefPtr<Gtk::Builder> refBuilder = Gtk::Builder::create();
+	try	{
+		refBuilder->add_from_file("basic.glade");
+	} catch(const Glib::FileError& ex){
+		std::cerr << "FileError: " << ex.what() << std::endl;
+		return 1;
+	} catch(const Glib::MarkupError& ex) {
+		std::cerr << "MarkupError: " << ex.what() << std::endl;
+		return 1;
+	} catch(const Gtk::BuilderError& ex) {
+		std::cerr << "BuilderError: " << ex.what() << std::endl;
+		return 1;
+	}
+
+	//Get the GtkBuilder-instantiated Dialog:
+	refBuilder->get_widget("DialogBasic", pDialog);
+	if(pDialog) {
+		//Get the GtkBuilder-instantiated Button, and connect a signal handler:
+		Gtk::Button* pButton = 0;
+		refBuilder->get_widget("quit_button", pButton);
+		if(pButton) {
+			pButton->signal_clicked().connect( sigc::ptr_fun(on_button_clicked) );
+		}
+		app->run(*pDialog);
+	}
+	delete pDialog;
+	return 0;
+}
